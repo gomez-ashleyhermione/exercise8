40a1dbc08a901853515377def88540d2b26811a6
ashley hermione gomez <hermionegomez49@gmail.com>
2024-11-09 09:30:45 +0800

    add codes

---
+++ ex8.py
@@ -0,0 +1,17 @@
+
+sum_of_numbers = 0
+
+while True:
+
+   number = int(input("Enter a number: "))
+
+   if number == 0:
+
+       break
+
+   sum_of_numbers += number
+
+print("Sum: ", sum_of_numbers, )
+
+
+      
